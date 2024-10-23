dbfd9d005550354e701d70e8be04b647d46f5ebd
enbraining <me@fodo.dev>
2024-10-23 11:16:41 +0900

    :recycle: 예외 처리 수정

--- src/main/java/com/gple/backend/global/exception/ExceptionAdvice.java
+++ src/main/java/com/gple/backend/global/exception/ExceptionAdvice.java
@@ -1,31 +1,31 @@
 package com.gple.backend.global.exception;
 
+import com.google.gson.Gson;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionAdvice extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         BindingResult result = ex.getBindingResult();
-        StringBuilder errMessage = new StringBuilder();
 
-        errMessage
-            .append("{\"status\":")
-            .append(HttpStatus.BAD_REQUEST.value())
-            .append(",")
-            .append("\"message\":\"")
-            .append(result.getFieldErrors().getFirst().getDefaultMessage())
-            .append("\"}");
+        Gson gson = new Gson();
+        Map<String, Object> response = new HashMap<>();
 
-        return new ResponseEntity<>(errMessage , HttpStatus.BAD_REQUEST);
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("message", result.getFieldErrors().getFirst().getDefaultMessage());
+
+        return new ResponseEntity<>(gson.toJson(response), HttpStatus.BAD_REQUEST);
     }
 }
