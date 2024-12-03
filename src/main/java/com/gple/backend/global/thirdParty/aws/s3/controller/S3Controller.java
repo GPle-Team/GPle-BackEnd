aaeffb7f237bcbfe3c622f58e6ad05b5021386e7
kimkyumbi <kimgyeombi@naver.com>
2024-12-03 14:05:24 +0900

    ✨AWS S3 추가

---
+++ src/main/java/com/gple/backend/global/thirdParty/aws/s3/controller/S3Controller.java
@@ -0,0 +1,34 @@
+package com.gple.backend.global.thirdParty.aws.s3.controller;
+
+import com.gple.backend.global.thirdParty.aws.s3.dto.response.UploadImageResDto;
+import com.gple.backend.global.thirdParty.aws.s3.dto.response.UploadImagesResDto;
+import com.gple.backend.global.thirdParty.aws.s3.service.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class S3Controller {
+    private final S3Service s3Service;
+
+    @PostMapping("/image")
+    public UploadImageResDto uploadImage(
+            @RequestParam(value = "file") MultipartFile multipartFile
+    ) {
+        String fileUrl = s3Service.uploadImage(multipartFile);
+        return new UploadImageResDto(fileUrl);
+    }
+
+    @PostMapping("/images")
+    public UploadImagesResDto uploadImages(
+            @RequestParam(value = "files") List<MultipartFile> multipartFiles
+    ) {
+        List<String> fileUrls = s3Service.uploadImages(multipartFiles);
+        return new UploadImagesResDto(fileUrls);
+    }
+}
---
+++ src/main/java/com/gple/backend/global/thirdParty/aws/s3/dto/response/UploadImageResDto.java
@@ -0,0 +1,6 @@
+package com.gple.backend.global.thirdParty.aws.s3.dto.response;
+
+public record UploadImageResDto (
+        String url
+) {
+}
