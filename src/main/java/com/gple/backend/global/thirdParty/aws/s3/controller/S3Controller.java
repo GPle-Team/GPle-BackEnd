5125049e2f6525194d23521c0841aae3ece50530
kimkyumbi <kimgyeombi@naver.com>
2024-12-18 16:50:59 +0900

    ♻️ url 변경

--- src/main/java/com/gple/backend/global/thirdParty/aws/s3/controller/S3Controller.java
+++ src/main/java/com/gple/backend/global/thirdParty/aws/s3/controller/S3Controller.java
@@ -5,6 +5,7 @@ import com.gple.backend.global.thirdParty.aws.s3.dto.response.UploadImagesResDto
 import com.gple.backend.global.thirdParty.aws.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
+@RequestMapping("/image")
 @RequiredArgsConstructor
 public class S3Controller {
     private final S3Service s3Service;
