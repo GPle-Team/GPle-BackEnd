1ad611307eb782b4b807752f62c2f8f5501cbbc4
enbraining <me@bricn.net>
2024-12-19 10:05:30 +0900

    :sparkles: 유저 프로필 작성 API

--- src/main/java/com/gple/backend/domain/user/controller/dto/web/request/CreateUserProfileRequest.java
+++ src/main/java/com/gple/backend/domain/user/controller/dto/web/request/CreateUserProfileRequest.java
@@ -6,21 +6,18 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@Builder
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CreateUserProfileRequest {
-    @Pattern(regexp = "^[가-힣]+$", message = "한글로 이루어진 본명을 입력해야합니다.")
-    private String username;
+    private String name;
 
-    @Pattern(regexp = "^[1-3][1-4][0-9]{2}$", message = "형식이 일치하지 않는 학번입니다.")
-    private String studentNumber;
+    private String number;
+
+    private MultipartFile file;
 }
