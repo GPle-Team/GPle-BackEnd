b810d5d8892e4577a9301af6eca1d7112e2fdb1a
enbraining <me@bricn.net>
2024-12-19 12:08:57 +0900

    :recycle: DTO 공통 로직 추가

--- src/main/java/com/gple/backend/domain/post/service/QueryAllReactedPostService.java
+++ src/main/java/com/gple/backend/domain/post/service/QueryAllReactedPostService.java
@@ -1,15 +1,9 @@
 package com.gple.backend.domain.post.service;
 
-import com.gple.backend.domain.emoji.controller.dto.response.EmojiResponse;
 import com.gple.backend.domain.emoji.entity.Emoji;
-import com.gple.backend.domain.emoji.entity.EmojiType;
 import com.gple.backend.domain.emoji.repository.EmojiRepository;
-import com.gple.backend.domain.post.controller.dto.common.AuthorResponse;
 import com.gple.backend.domain.post.controller.dto.presentation.response.QueryPostResponse;
 import com.gple.backend.domain.post.entity.Post;
-import com.gple.backend.domain.post.repository.PostRepository;
-import com.gple.backend.domain.tag.dto.response.TagResponse;
-import com.gple.backend.domain.tag.entity.Tag;
 import com.gple.backend.domain.user.entity.User;
 import com.gple.backend.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +12,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static com.gple.backend.domain.post.common.PostCommon.getEmojiCount;
+import static com.gple.backend.domain.post.common.PostCommon.postListToDto;
 
 @Service
 @RequiredArgsConstructor
 public class QueryAllReactedPostService {
-    private final PostRepository postRepository;
     private final UserUtil userUtil;
     private final EmojiRepository emojiRepository;
 
@@ -37,39 +30,6 @@ public class QueryAllReactedPostService {
             .distinct()
             .toList();
 
-        return reactedPosts.stream().map(post -> {
-            List<Tag> tags = post.getTag();
-            List<TagResponse> tagDtoList = tags.stream().map(tag -> TagResponse.builder()
-                .id(tag.getUser().getId())
-                .name(tag.getUser().getName())
-                .build()
-            ).toList();
-
-            List<Emoji> emojis = post.getEmoji();
-            EmojiResponse emojiDtoList = EmojiResponse.builder()
-                .heartCount(getEmojiCount(emojis, EmojiType.HEART))
-                .thumbsCount(getEmojiCount(emojis, EmojiType.THUMBSUP))
-                .thinkCount(getEmojiCount(emojis, EmojiType.THINKING))
-                .poopCount(getEmojiCount(emojis, EmojiType.POOP))
-                .chinaCount(getEmojiCount(emojis, EmojiType.CHINA))
-                .congCount(getEmojiCount(emojis, EmojiType.CONGRATULATION))
-                .build();
-
-            return QueryPostResponse.builder()
-                .author(AuthorResponse.builder()
-                    .grade(post.getUser().getGrade())
-                    .name(post.getUser().getName())
-                    .id(post.getUser().getId())
-                    .build()
-                )
-                .id(post.getId())
-                .title(post.getTitle())
-                .location(post.getLocation())
-                .tagList(tagDtoList)
-                .createdTime(post.getCreatedTime())
-                .emojiList(emojiDtoList)
-                .imageUrl(post.getImageUrl())
-                .build();
-        }).toList();
+        return postListToDto(reactedPosts);
     }
 }
