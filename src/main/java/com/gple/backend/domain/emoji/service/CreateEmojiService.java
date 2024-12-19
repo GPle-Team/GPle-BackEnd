89602efaa7e52d73943ab9df0d4c025b72097016
enbraining <me@bricn.net>
2024-12-19 10:02:44 +0900

    :truck: EmojiService to CreateEmojiService

--- src/main/java/com/gple/backend/domain/emoji/service/CreateEmojiService.java
+++ src/main/java/com/gple/backend/domain/emoji/service/CreateEmojiService.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class EmojiService {
+public class CreateEmojiService {
     private final EmojiRepository emojiRepository;
     private final PostRepository postRepository;
     private final UserUtil userUtil;
