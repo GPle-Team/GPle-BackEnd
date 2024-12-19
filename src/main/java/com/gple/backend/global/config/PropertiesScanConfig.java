5305ab16d024df1f1b16774342a49f97e0240a20
enbraining <me@bricn.net>
2024-12-19 23:04:56 +0900

    :recycle: 애노테이션 변경 (클라우드타입으로 인한)

--- src/main/java/com/gple/backend/global/config/PropertiesScanConfig.java
+++ src/main/java/com/gple/backend/global/config/PropertiesScanConfig.java
@@ -1,10 +1,11 @@
 package com.gple.backend.global.config;
 
+import com.gple.backend.global.thirdParty.aws.properties.S3Properties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationPropertiesScan(basePackages = {
-        "com.gple.backend.global.thirdParty.aws.properties"})
+@EnableConfigurationProperties(S3Properties.class)
 public class PropertiesScanConfig {
 }
