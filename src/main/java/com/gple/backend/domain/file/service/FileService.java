a167a3d8508794011ce41b9da3873c33c126afbe
enbraining <me@bricn.net>
2024-12-20 14:45:12 +0900

    이미지 리사이징 로직 추가

--- src/main/java/com/gple/backend/domain/file/service/FileService.java
+++ src/main/java/com/gple/backend/domain/file/service/FileService.java
@@ -1,6 +1,7 @@
 package com.gple.backend.domain.file.service;
 
 import com.gple.backend.global.exception.ExpectedException;
+import com.gple.backend.global.file.ImageMultipartFile;
 import com.gple.backend.global.thirdParty.aws.properties.S3Properties;
 import io.awspring.cloud.s3.ObjectMetadata;
 import io.awspring.cloud.s3.S3Exception;
@@ -9,14 +10,16 @@ import io.awspring.cloud.s3.S3Template;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.core.exception.SdkClientException;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -28,7 +31,6 @@ public class FileService {
     private final S3Template s3Template;
     private final S3Properties properties;
 
-    @Transactional
     public String uploadImage(MultipartFile multipartFile) {
         if (multipartFile.isEmpty()) {
             throw new ExpectedException("파일이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
@@ -37,6 +39,12 @@ public class FileService {
         String fileExtension = StringUtils.getFilenameExtension(originalFileName);
         validateFileExtensionType(Objects.requireNonNull(fileExtension));
         String fileName = generateFileName(fileExtension);
+
+        try {
+            multipartFile = resizeImage(multipartFile);
+        } catch (IOException e){
+            throw new ExpectedException("파일 사이즈를 변환하는 과정에서 문제가 생겼습니다.", HttpStatus.BAD_REQUEST);
+        }
 
         try {
             S3Resource s3Resource = s3Template.upload(
@@ -58,7 +66,6 @@ public class FileService {
         }
     }
 
-    @Transactional
     public List<String> uploadImages(List<MultipartFile> multipartFiles) {
         if (multipartFiles.isEmpty()) {
             throw new ExpectedException("파일이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
@@ -98,7 +105,7 @@ public class FileService {
     }
 
     private String generateFileName(String fileExtension) {
-        return UUID.randomUUID().toString() + LocalDateTime.now() + "." + fileExtension;
+        return UUID.randomUUID() + "." + fileExtension;
     }
 
     private void validateFileExtensionType(String fileExtention) {
@@ -106,5 +113,34 @@ public class FileService {
         if (!allowExtensions.contains(fileExtention.toLowerCase())) {
             throw new ExpectedException("지원하지 않는 파일 확장자입니다.", HttpStatus.BAD_REQUEST);
         }
+    }
+
+    private MultipartFile resizeImage(MultipartFile imageFile) throws IOException {
+        BufferedImage image = ImageIO.read(imageFile.getInputStream());
+
+        int originWidth = image.getWidth();
+        int originHeight = image.getHeight();
+
+        int changedWidth, changedHeight;
+        if(originWidth > originHeight){
+            changedWidth = 390;
+            changedHeight = (originHeight * 390) / originWidth;
+        } else {
+            changedHeight = 390;
+            changedWidth = (originWidth * 390) / originHeight;
+        }
+
+        Image newImage = image.getScaledInstance(changedWidth, changedHeight, Image.SCALE_SMOOTH);
+        BufferedImage resizedImage = new BufferedImage(changedWidth, changedHeight, BufferedImage.TYPE_INT_RGB);
+
+        Graphics graphics = resizedImage.getGraphics();
+        graphics.drawImage(newImage, 0, 0, null);
+        graphics.dispose();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(resizedImage, "jpg", outputStream);
+        byte[] imageByte = outputStream.toByteArray();
+
+        return new ImageMultipartFile(imageByte, generateFileName("jpg"), "image/jpeg");
     }
 }
