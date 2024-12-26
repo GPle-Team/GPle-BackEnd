56234d33ad96d028648aad02b201c27a59a337ac
kimkyumbi <kimgyeombi@naver.com>
2024-12-26 09:38:37 +0900

    ♻️ EmojiType, postId로 이모지 찾기

--- src/main/java/com/gple/backend/domain/emoji/repository/EmojiRepository.java
+++ src/main/java/com/gple/backend/domain/emoji/repository/EmojiRepository.java
@@ -2,14 +2,13 @@ package com.gple.backend.domain.emoji.repository;
 
 
 import com.gple.backend.domain.emoji.entity.Emoji;
+import com.gple.backend.domain.emoji.entity.EmojiType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EmojiRepository extends JpaRepository<Emoji, Long> {
-    List<Emoji> findEmojiByPostId(Long postId);
+    Emoji findEmojiByEmojiTypeAndPostId(EmojiType emojiType, Long postId);
 
     List<Emoji> findByUserId(Long userId);
 }
