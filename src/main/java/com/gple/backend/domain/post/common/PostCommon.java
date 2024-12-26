7d3a8fb5699034c907058886bfbda4018a8e016b
kimkyumbi <kimgyeombi@naver.com>
2024-12-26 10:33:46 +0900

    EnumType 이름 변경

--- src/main/java/com/gple/backend/domain/post/common/PostCommon.java
+++ src/main/java/com/gple/backend/domain/post/common/PostCommon.java
@@ -36,19 +36,19 @@ public class PostCommon {
         List<Emoji> emojis = post.getEmoji();
         EmojiResponse emojiDtoList = EmojiResponse.builder()
             .heartCount(getEmojiCount(emojis, EmojiType.HEART))
-            .thumbsCount(getEmojiCount(emojis, EmojiType.THUMBSUP))
-            .thinkCount(getEmojiCount(emojis, EmojiType.THINKING))
-            .poopCount(getEmojiCount(emojis, EmojiType.POOP))
-            .chinaCount(getEmojiCount(emojis, EmojiType.CHINA))
-            .congCount(getEmojiCount(emojis, EmojiType.CONGRATULATION))
+            .thumbsCount(getEmojiCount(emojis, EmojiType.CONGRATS))
+            .thinkCount(getEmojiCount(emojis, EmojiType.THUMBSUP))
+            .poopCount(getEmojiCount(emojis, EmojiType.THINKING))
+            .chinaCount(getEmojiCount(emojis, EmojiType.POOP))
+            .congCount(getEmojiCount(emojis, EmojiType.CHINA))
             .build();
 
         List<Boolean> checkEmoji = List.of(
             getMyReactedEmoji(emojis, EmojiType.HEART, userId),
+            getMyReactedEmoji(emojis, EmojiType.CONGRATS, userId),
             getMyReactedEmoji(emojis, EmojiType.THUMBSUP, userId),
             getMyReactedEmoji(emojis, EmojiType.THINKING, userId),
             getMyReactedEmoji(emojis, EmojiType.POOP, userId),
-            getMyReactedEmoji(emojis, EmojiType.CONGRATULATION, userId),
             getMyReactedEmoji(emojis, EmojiType.CHINA, userId)
         );
 
